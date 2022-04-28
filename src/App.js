1f444b987db6040c603d6be7a0161f708a96ef4a
mahfuz47 <mahfuzshikder560@gmail.com>
2022-04-28 18:59:21 +0600

    update user and delete user added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './component/Home/Home';
 import AddUser from './component/AddUser/AddUser';
+import UpdateUser from './component/UpdateUser/UpdateUser';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
      <Routes>
        <Route path='user/add' element={<AddUser></AddUser>}></Route>
        <Route path='/' element={<Home></Home>}></Route>
+       <Route path='/update/:id' element={<UpdateUser></UpdateUser>}></Route>
      </Routes>
     </div>
   );
--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     const [users, setUsers] = useState([]);
@@ -9,16 +10,32 @@ const Home = () => {
     },[])
 
     const handleDeleteUser = id => {
-
+      const proceed = window.confirm('are you sure you want to delete?');
+      if(proceed){
+        console.log('deleting user with id', id);
+        const url = `http://localhost:5000/user/${id}`;
+        fetch(url, {
+          method: 'DELETE'
+        })
+        .then(res => res.json())
+        .then(data => {
+          if(data.deletedCount > 0){
+            console.log('deleted');
+            const remaining = users.filter(user => user._id !== id);
+            setUsers(remaining);
+          }
+        })
+      }
     }
     return (
       <div>
         <h2>Available Users: {users.length}</h2>
         <ul>
           {users.map((user) => (
-            <li>
+            <li key={user._id}>
               {user.name} :: {user.email}{" "}
-              <button onClick={handleDeleteUser(user._id)}>X</button>
+              <Link to={`/update/${user._id}`}><button>Update</button></Link>
+              <button onClick={()=>handleDeleteUser(user._id)}>X</button>
             </li>
           ))}
         </ul>
